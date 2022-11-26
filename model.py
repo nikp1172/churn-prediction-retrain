366e2c3392917e7254cc24180096394fc2d555f1
Arsh <lucifer78908@gmail.com>
2022-11-26 12:49:20 +0530

    Updated Data Path
    Signed-off-by: Arsh <lucifer78908@gmail.com>

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 def train_model():
     df = pd.read_csv(
-        'Churn_Modelling.csv')
+        'Data/Churn_Modelling.csv')
     X = df.iloc[:, 3:-1].drop(['Geography', 'Gender'], axis=1)
     y = df.iloc[:, -1]
     classifier = Classification(predictor = ['rfc'])
